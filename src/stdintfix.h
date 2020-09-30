6cd19ec95cac70ee36be9e13a7dc34b18b338bd3
David Newman <dnewman@twitter.com>
2020-09-29 21:42:18 -0700

    work in progress

--- src/stdintfix.h
+++ src/stdintfix.h
@@ -45,8 +45,11 @@ typedef signed short int int16_t;
 typedef unsigned short int uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
+
+#ifdef __linux__
 typedef signed long int int64_t;
 typedef unsigned long int uint64_t;
+#endif
 
 typedef unsigned long int	uintptr_t;
 
